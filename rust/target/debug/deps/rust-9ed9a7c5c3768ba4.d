3ec1fbc209db2bc73d3bd17ca4cd495be142b03d
LaiVoJM <84391289+LaiVoJM@users.noreply.github.com>
2022-10-20 16:14:27 +0700

    first

---
+++ rust/target/debug/deps/rust-9ed9a7c5c3768ba4.d
@@ -0,0 +1,5 @@
+d:\LaiVo\Rust\rust_assignment_1\rust\target\debug\deps\rust-9ed9a7c5c3768ba4.rmeta: src\main.rs
+
+d:\LaiVo\Rust\rust_assignment_1\rust\target\debug\deps\rust-9ed9a7c5c3768ba4.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/debug/deps/rust-9f16adf20ac7e6c8.d
@@ -0,0 +1,5 @@
+d:\LaiVo\Rust\rust_assignment_1\rust\target\debug\deps\rust-9f16adf20ac7e6c8.rmeta: src\main.rs
+
+d:\LaiVo\Rust\rust_assignment_1\rust\target\debug\deps\rust-9f16adf20ac7e6c8.d: src\main.rs
+
+src\main.rs:
